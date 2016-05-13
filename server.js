2fce868e5d819e853ead7c6f8036c9dce1e47ec8
lizannbrowne <lizannbrowne@gmail.com>
2016-05-13 11:31:18 -0700

    devops

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var path = require('path');
+var httpProxy = require('http-proxy');
+
+var proxy = httpProxy.createProxyServer({
+  changeOrigin: true
+});
+var app = express();
+
+var isProduction = process.env.NODE_ENV === 'production';
+var port = isProduction ? process.env.PORT : 3000;
+var publicPath = path.resolve(__dirname, 'public');
+
+app.use(express.static(publicPath));
+
+// If you only want this for development, you would of course
+// put it in the "if" block below
+app.all('/db/*', function (req, res) {
+  proxy.web(req, res, {
+    target: 'https://flickerfilmstats.firebaseio.com/'
+  });
+});
+
+// We only want to run the workflow when not in production
+if (!isProduction) {
+  var bundle = require('./server/bundle.js');
+   bundle();
+   app.all('/build/*', function (req, res) {
+     proxy.web(req, res, {
+         target: 'http://localhost:8080'
+     });
+   });
+}
+
+proxy.on('error', function(e) {
+  console.log('Could not connect to proxy, please try again...');
+});
+
+app.listen(port, function () {
+  console.log('Server running on port ' + port);
+});
